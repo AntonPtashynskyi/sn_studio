bc5da0f5a506e8eb0ab6f487a46806ead0d8cd5f
unknown <a.ptashysnkyi@gmail.com>
2024-05-20 23:46:46 +0200

    Add Google maps

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import Map from "../Maps/Maps";
 
 import instagramLogo from "../../../public/assets/svg/instagram--in_brows.svg";
 import emailLogo from "../../../public/assets/svg/email--logo.svg";
@@ -8,35 +9,38 @@ import "./contact.scss";
 
 export const Contact = () => {
   return (
-    <div className="contact-wrapper">
-      <Link
-        href="https://www.instagram.com/in_brows__/"
-        className="contact-link"
-      >
-        <Image
-          src={instagramLogo}
-          width={40}
-          height={40}
-          alt="In brows instagram"
-          className="image-contact"
-        />
-        <p className="text-[20px] font-light text-black">in_brows__</p>
-      </Link>
-      <Link
-        href="https://www.instagram.com/in_brows__/"
-        className="contact-link"
-      >
-        <Image
-          src={emailLogo}
-          width={40}
-          height={40}
-          alt="In brows instagram"
-          className="image-contact"
-        />
-        <p className="text-[20px] font-light text-black">
-          snissarinna@gmail.com
-        </p>
-      </Link>
-    </div>
+    <>
+      <div className="contact-wrapper">
+        <Link
+          href="https://www.instagram.com/in_brows__/"
+          className="contact-link"
+        >
+          <Image
+            src={instagramLogo}
+            width={40}
+            height={40}
+            alt="In brows instagram"
+            className="image-contact"
+          />
+          <p className="text-[20px] font-light text-black">in_brows__</p>
+        </Link>
+        <Link
+          href="https://www.instagram.com/in_brows__/"
+          className="contact-link"
+        >
+          <Image
+            src={emailLogo}
+            width={40}
+            height={40}
+            alt="In brows instagram"
+            className="image-contact"
+          />
+          <p className="text-[20px] font-light text-black">
+            snissarinna@gmail.com
+          </p>
+        </Link>
+      </div>
+      <Map />
+    </>
   );
 };
---
+++ src/components/Maps/Maps.jsx
@@ -0,0 +1,43 @@
+"use client";
+import { useEffect, useRef, useMemo } from "react";
+import { Loader } from "@googlemaps/js-api-loader";
+
+function Map({ address }) {
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    const initMap = async () => {
+      const loader = new Loader({
+        apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+        version: "weekly",
+        libraries: ["places"],
+      });
+
+      const { Map } = await loader.importLibrary("maps");
+      const { AdvancedMarkerElement } = await loader.importLibrary("marker");
+
+      const position = {
+        lat: 51.07601158716394,
+        lng: 17.03834648611571,
+      };
+
+      const mapOptions = {
+        center: position,
+        zoom: 15,
+        mapId: "In_brows",
+      };
+
+      const map = new Map(mapRef.current, mapOptions);
+      const marker = new AdvancedMarkerElement({
+        map: map,
+        position: position,
+        title: "In Brows",
+      });
+    };
+
+    initMap();
+  }, [address]);
+
+  return <div className="h-96 rounded-[3rem] mt-10" ref={mapRef} />;
+}
+export default Map;
