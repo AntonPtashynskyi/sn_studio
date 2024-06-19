a7c963b96393fb95926c73e02acc25a10ff69903
Anton Ptashynskyi <aptashysnkyi@opera.com>
2024-06-19 14:05:15 +0200

    Dockarize, prepare for deploy

--- next.config.js
+++ next.config.js
@@ -13,8 +13,7 @@ const nextConfig = {
   env: {
     GOOGLE_SERVICE_PRIVATE_KEY: process.env.GOOGLE_SERVICE_PRIVATE_KEY,
   },
-  output: "export",
-  basePath: "/sn_studio"
+  output: "standalone",
 }
 
 module.exports = nextConfig
